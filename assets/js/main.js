1e1f24e11b7a9f3ecb045f3b07b69f79d5df21fe
hosseinamoozegar <amoozegar96@gmail.com>
2021-04-20 17:40:26 +0430

    fix bugs and clean-code

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,15 @@ $(".menu-btn").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
 });
-
+//----------------popovers-----------------
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+});
+//----------------popovers-----------------
+//----------------tooltip-----------------
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+//----------------tooltip-----------------
