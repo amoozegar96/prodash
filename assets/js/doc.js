07623bb5c27f53e68c723215fec5c86ced127554
A.Shariyat <a.shariyat.78.99.20@gmail.com>
2021-04-22 18:02:51 +0430

    partitioning pages.

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,3 +1,28 @@
+//----------------popovers-----------------
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+});
+//----------------popovers-----------------
+//----------------tooltip-----------------
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+//----------------tooltip-----------------
+//----------------toast-----------------
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl)
+})
+var liveToastBtn = document.getElementById('liveToastBtn'),
+    liveToast = document.getElementById('liveToast');
+liveToastBtn.addEventListener('click', function () {
+    var showToast = new bootstrap.Toast(liveToast);
+    showToast.show()
+})
+//----------------toast-----------------
+//----------------charts-----------------
 am4core.ready(function() {
 
 // Themes begin
@@ -290,3 +315,8 @@ am4core.ready(function() {
     }
 
 }); // end am4core.ready()
+//----------------charts-----------------
+//----------------summernote-----------------
+SUNEDITOR.create('editor');
+
+//----------------summernote-----------------
--- assets/js/main.js
+++ assets/js/main.js
@@ -27,27 +27,3 @@ $(".menu-btn").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
 });
-//----------------popovers-----------------
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-});
-//----------------popovers-----------------
-//----------------tooltip-----------------
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-});
-//----------------tooltip-----------------
-//----------------toast-----------------
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl)
-})
-var liveToastBtn = document.getElementById('liveToastBtn'),
-liveToast = document.getElementById('liveToast');
-liveToastBtn.addEventListener('click', function () {
-    var showToast = new bootstrap.Toast(liveToast);
-    showToast.show()
-})
-//----------------toast-----------------
