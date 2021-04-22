9773ec3575e198ab2cb3a6e4b63d66039aa6d042
A.Shariyat <a.shariyat.78.99.20@gmail.com>
2021-04-22 12:23:21 +0430

    add chart js.

---
+++ assets/js/charts.js
@@ -0,0 +1,292 @@
+am4core.ready(function() {
+
+// Themes begin
+    am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+    var chart = am4core.create("chartdiv", am4charts.RadarChart);
+    chart.scrollbarX = new am4core.Scrollbar();
+
+    var data = [];
+
+    for(var i = 0; i < 20; i++){
+        data.push({category: i, value:Math.round(Math.random() * 100)});
+    }
+
+    chart.data = data;
+    chart.radius = am4core.percent(100);
+    chart.innerRadius = am4core.percent(50);
+
+// Create axes
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.tooltip.disabled = true;
+    categoryAxis.renderer.minHeight = 110;
+    categoryAxis.renderer.grid.template.disabled = true;
+//categoryAxis.renderer.labels.template.disabled = true;
+    let labelTemplate = categoryAxis.renderer.labels.template;
+    labelTemplate.radius = am4core.percent(-60);
+    labelTemplate.location = 0.5;
+    labelTemplate.relativeRotation = 90;
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.grid.template.disabled = true;
+    valueAxis.renderer.labels.template.disabled = true;
+    valueAxis.tooltip.disabled = true;
+
+// Create series
+    var series = chart.series.push(new am4charts.RadarColumnSeries());
+    series.sequencedInterpolation = true;
+    series.dataFields.valueY = "value";
+    series.dataFields.categoryX = "category";
+    series.columns.template.strokeWidth = 0;
+    series.tooltipText = "{valueY}";
+    series.columns.template.radarColumn.cornerRadius = 10;
+    series.columns.template.radarColumn.innerCornerRadius = 0;
+
+    series.tooltip.pointerOrientation = "vertical";
+
+// on hover, make corner radiuses bigger
+    let hoverState = series.columns.template.radarColumn.states.create("hover");
+    hoverState.properties.cornerRadius = 0;
+    hoverState.properties.fillOpacity = 1;
+
+
+    series.columns.template.adapter.add("fill", function(fill, target) {
+        return chart.colors.getIndex(target.dataItem.index);
+    })
+
+// Cursor
+    chart.cursor = new am4charts.RadarCursor();
+    chart.cursor.innerRadius = am4core.percent(50);
+    chart.cursor.lineY.disabled = true;
+
+}); // end am4core.ready()
+am4core.ready(function() {
+
+// Themes begin
+    am4core.useTheme(am4themes_animated);
+// Themes end
+
+
+
+// Create chart instance
+    var chart = am4core.create("chartdiv2", am4charts.RadarChart);
+
+// Add data
+    chart.data = [{
+        "category": "Research",
+        "value": 80,
+        "full": 100
+    }, {
+        "category": "Marketing",
+        "value": 35,
+        "full": 100
+    }, {
+        "category": "Distribution",
+        "value": 92,
+        "full": 100
+    }, {
+        "category": "Human Resources",
+        "value": 68,
+        "full": 100
+    }];
+
+// Make chart not full circle
+    chart.startAngle = -90;
+    chart.endAngle = 180;
+    chart.innerRadius = am4core.percent(20);
+
+// Set number format
+    chart.numberFormatter.numberFormat = "#.#'%'";
+
+// Create axes
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.grid.template.strokeOpacity = 0;
+    categoryAxis.renderer.labels.template.horizontalCenter = "right";
+    categoryAxis.renderer.labels.template.fontWeight = 500;
+    categoryAxis.renderer.labels.template.adapter.add("fill", function(fill, target) {
+        return (target.dataItem.index >= 0) ? chart.colors.getIndex(target.dataItem.index) : fill;
+    });
+    categoryAxis.renderer.minGridDistance = 10;
+
+    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.grid.template.strokeOpacity = 0;
+    valueAxis.min = 0;
+    valueAxis.max = 100;
+    valueAxis.strictMinMax = true;
+
+// Create series
+    var series1 = chart.series.push(new am4charts.RadarColumnSeries());
+    series1.dataFields.valueX = "full";
+    series1.dataFields.categoryY = "category";
+    series1.clustered = false;
+    series1.columns.template.fill = new am4core.InterfaceColorSet().getFor("alternativeBackground");
+    series1.columns.template.fillOpacity = 0.08;
+    series1.columns.template.cornerRadiusTopLeft = 20;
+    series1.columns.template.strokeWidth = 0;
+    series1.columns.template.radarColumn.cornerRadius = 20;
+
+    var series2 = chart.series.push(new am4charts.RadarColumnSeries());
+    series2.dataFields.valueX = "value";
+    series2.dataFields.categoryY = "category";
+    series2.clustered = false;
+    series2.columns.template.strokeWidth = 0;
+    series2.columns.template.tooltipText = "{category}: [bold]{value}[/]";
+    series2.columns.template.radarColumn.cornerRadius = 20;
+
+    series2.columns.template.adapter.add("fill", function(fill, target) {
+        return chart.colors.getIndex(target.dataItem.index);
+    });
+
+// Add cursor
+    chart.cursor = new am4charts.RadarCursor();
+
+}); // end am4core.ready()
+am4core.ready(function() {
+
+// Themes begin
+    am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+    var chart = am4core.create("chartdiv3", am4charts.XYChart);
+
+// Add data
+    chart.data = [{
+        "country": "USA",
+        "visits": 2025
+    }, {
+        "country": "China",
+        "visits": 1882
+    }, {
+        "country": "Japan",
+        "visits": 1809
+    }, {
+        "country": "Germany",
+        "visits": 1322
+    }, {
+        "country": "UK",
+        "visits": 1122
+    }, {
+        "country": "France",
+        "visits": 1114
+    }, {
+        "country": "India",
+        "visits": 984
+    }, {
+        "country": "Spain",
+        "visits": 711
+    }, {
+        "country": "Netherlands",
+        "visits": 665
+    }, {
+        "country": "Russia",
+        "visits": 580
+    }, {
+        "country": "South Korea",
+        "visits": 443
+    }, {
+        "country": "Canada",
+        "visits": 441
+    }, {
+        "country": "Brazil",
+        "visits": 395
+    }];
+
+// Create axes
+
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "country";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+
+    categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
+        if (target.dataItem && target.dataItem.index & 2 == 2) {
+            return dy + 25;
+        }
+        return dy;
+    });
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.valueY = "visits";
+    series.dataFields.categoryX = "country";
+    series.name = "Visits";
+    series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/]";
+    series.columns.template.fillOpacity = .8;
+
+    var columnTemplate = series.columns.template;
+    columnTemplate.strokeWidth = 2;
+    columnTemplate.strokeOpacity = 1;
+
+}); // end am4core.ready()
+am4core.ready(function() {
+
+// Themes begin
+    am4core.useTheme(am4themes_animated);
+// Themes end
+
+// Create chart instance
+    var chart = am4core.create("chartdiv4", am4charts.XYChart);
+
+// Add data
+    chart.data = generateChartData();
+
+// Create axes
+    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 50;
+
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+// Create series
+    var series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = "visits";
+    series.dataFields.dateX = "date";
+    series.strokeWidth = 2;
+    series.minBulletDistance = 10;
+    series.tooltipText = "{valueY}";
+    series.tooltip.pointerOrientation = "vertical";
+    series.tooltip.background.cornerRadius = 20;
+    series.tooltip.background.fillOpacity = 0.5;
+    series.tooltip.label.padding(12,12,12,12)
+
+// Add scrollbar
+    chart.scrollbarX = new am4charts.XYChartScrollbar();
+    chart.scrollbarX.series.push(series);
+
+// Add cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.xAxis = dateAxis;
+    chart.cursor.snapToSeries = series;
+
+    function generateChartData() {
+        var chartData = [];
+        var firstDate = new Date();
+        firstDate.setDate(firstDate.getDate() - 1000);
+        var visits = 1200;
+        for (var i = 0; i < 500; i++) {
+            // we create date objects here. In your data, you can have date strings
+            // and then set format of your dates using chart.dataDateFormat property,
+            // however when possible, use date objects, as this will speed up chart rendering.
+            var newDate = new Date(firstDate);
+            newDate.setDate(newDate.getDate() + i);
+
+            visits += Math.round((Math.random()<0.5?1:-1)*Math.random()*10);
+
+            chartData.push({
+                date: newDate,
+                visits: visits
+            });
+        }
+        return chartData;
+    }
+
+}); // end am4core.ready()
